83dc4657bc1267dcaeb8dcee3d205c3d02aeb5f2
Alexey Voznyuk <me@swizard.info>
2012-07-14 01:21:31 +0400

    robot move right sample

--- robot.lisp
+++ robot.lisp
@@ -1,3 +1,19 @@
 
 (in-package :lambda-lifter)
 
+(defun robot-move-right (world objects)
+  (let* ((robot-coords (first (funcall objects :robot)))
+         (rx (realpart robot-coords))
+         (ry (imagpart robot-coords)))
+    (values (lambda (x y)
+              (cond ((and (= x (1+ rx))
+                          (= y ry))
+                     :robot)
+                    ((and (= x rx) (= y ry)) nil)
+                    (t (funcall world x y))))
+            (lambda (type)
+              (case type
+                (:robot (list (complex (1+ rx) ry)))
+                (t (funcall objects type)))))))
+                    
+
