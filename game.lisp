6af14cbdf7a16127408604280a544d2f8fe57d84
Alexey Voznyuk <me@swizard.info>
2012-07-14 03:15:13 +0400

    closest lambda detection

--- game.lisp
+++ game.lisp
@@ -1,3 +1,11 @@
 
 (in-package :lambda-lifter)
 
+(defun find-closest-lambda (objects)
+  (iter (with robot-coords = (first (funcall objects :robot)))
+        (for lambda-coords in (funcall objects :lambda))
+        (for x-diff = (- (realpart robot-coords) (realpart lambda-coords)))
+        (for y-diff = (- (imagpart robot-coords) (imagpart lambda-coords)))
+        (for sq-distance = (+ (* x-diff x-diff) (* y-diff y-diff)))
+        (finding lambda-coords minimizing sq-distance)))
+
