963683447e11852a48779bdf3b414a33084f2b3b
Azamat S. Kalimoulline <turtle@bazon.ru>
2012-07-13 21:38:58 +0400

    Function to read as initial array.

--- map-builder.lisp
+++ map-builder.lisp
@@ -1,6 +1,18 @@
 
 (in-package :lambda-lifter)
 
+(defun array-map-builder ()
+  (let ((initial-world (make-array '(0 0) :adjustable t)))
+    (lambda (command &optional type x y)
+      (ecase command
+	(:build (destructuring-bind (x-size y-size)
+		    (array-dimensions initial-world)
+		  (when (or (> x x-size )
+			    (> y y-size))
+		    (adjust-array initial-world (list (max y y-size) (max x x-size))))
+		  (setf (aref initial-world (- y 1) (- x 1)) type)))
+	(:receive initial-world)))))
+
 (defun apply-map-parser (stream cell-receiver)
   (iter (for line in-stream stream using #'read-line)
         (for row-index from 1)
@@ -8,7 +20,7 @@
               (for cell-index from 1)
               (maximizing cell-index into max-cell-index)
               (assert (and max-cell-index (<= cell-index max-cell-index)))
-              (funcall cell-receiver
+              (funcall cell-receiver :build
                        (ecase char
                          (#\R :robot)
                          (#\# :wall)
@@ -20,4 +32,8 @@
                        cell-index
                        row-index))))
 
-
+(defun load-map (file)
+  (let ((builder (array-map-builder)))
+    (with-open-file (s file)
+      (apply-map-parser s builder)
+      (funcall builder :receive))))
