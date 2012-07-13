3f0ede881f4821acfc276e8a3b08708c5b36fd18
Alexey Voznyuk <me@swizard.info>
2012-07-14 01:37:27 +0400

    all robot moves

--- robot.lisp
+++ robot.lisp
@@ -1,19 +1,26 @@
 
 (in-package :lambda-lifter)
 
-(defun robot-move-right (world objects)
-  (let* ((robot-coords (first (funcall objects :robot)))
-         (rx (realpart robot-coords))
-         (ry (imagpart robot-coords)))
-    (values (lambda (x y)
-              (cond ((and (= x (1+ rx))
-                          (= y ry))
-                     :robot)
-                    ((and (= x rx) (= y ry)) nil)
-                    (t (funcall world x y))))
-            (lambda (type)
-              (case type
-                (:robot (list (complex (1+ rx) ry)))
-                (t (funcall objects type)))))))
+(defmacro defrobot-move (name delta-x delta-y)
+  `(defun ,name (world objects)
+     (let* ((robot-coords (first (funcall objects :robot)))
+            (rx (realpart robot-coords))
+            (ry (imagpart robot-coords)))
+       (values (lambda (x y)
+                 (cond ((and (= x (+ rx ,delta-x))
+                             (= y (+ ry ,delta-y)))
+                        :robot)
+                       ((and (= x rx) (= y ry)) nil)
+                       (t (funcall world x y))))
+               (lambda (type)
+                 (case type
+                   (:robot (list (complex (+ rx ,delta-x) (+ ry ,delta-y))))
+                   (t (funcall objects type))))))))
+
+(defrobot-move robot-move-left -1 0)
+(defrobot-move robot-move-right 1 0)
+(defrobot-move robot-move-up 0 1)
+(defrobot-move robot-move-down 0 -1)
+
                     
 
